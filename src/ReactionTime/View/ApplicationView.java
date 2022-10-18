610eb3b351b267c2bf80a77b64b4da812dc07ffb
Jakub Zabek <l6b7@mail.com>
2022-10-18 04:14:24 +0200

    Added View class

---
+++ src/ReactionTime/View/ApplicationView.java
@@ -0,0 +1,21 @@
+package ReactionTime.View;
+
+import javax.swing.JFrame;
+
+public class ApplicationView extends JFrame{
+	
+	public static void main(String[] args) {
+		ApplicationView a = new ApplicationView();
+	}
+	
+	public ApplicationView() {
+		setTitle("Reaction Time Test");
+		setSize(400, 400);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	
+	
+	
+}
