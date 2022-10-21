6eb8ad1afef898e64218d97e90ab9accb927186e
Jakub Zabek <l6b7@mail.com>
2022-10-21 09:38:38 +0200

    Connected View with the controller Cleaned up the code Spacebar now can be used to measure reaction time

--- src/ReactionTime/View/ApplicationView.java
+++ src/ReactionTime/View/ApplicationView.java
@@ -8,6 +8,9 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JButton;
@@ -17,12 +20,11 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 import javax.swing.border.BevelBorder;
 
-
 import ReactionTime.ReactionTimeApplication;
 import ReactionTime.Controller.Controller;//remove later
 
 public class ApplicationView {
-	
+
 	private JFrame frame;
 
 	private CardLayout cardLayout;
@@ -59,24 +61,24 @@ public class ApplicationView {
 	private final String WINDOW_NAME = "Reaction Time Test";
 	private final Dimension WINDOW_SIZE = new Dimension(800, 600);
 	private final Dimension PANEL_BAR_SIZE = new Dimension(0, 40);
-	
+
 	private final String CARD_NAME_START_PANEL = "startPanel";
 	private final String CARD_NAME_GET_READY_PANEL = "getReadyPanel";
 	private final String CARD_NAME_FALSE_START_PANEL = "falseStartPanel";
 	private final String CARD_NAME_CLICK_PANEL = "clickPanel";
-	
+
 	private final String CARD_NAME_APP_PANEL = "appPanel";
 	private final String CARD_NAME_RECORDS_PANEL = "RecordsPanel";
-	
+
 	private final Dimension BUTTON_SIZE = new Dimension(105, 30);
 	private final Dimension BUTTON_SIZE_WIDE = new Dimension(120, 30);
-	
+
 	private final String BUTTON_NAME_GO_TO_APP = "Application";
 	private final String BUTTON_NAME_REMOVE_LAST = "Remove Last";
 	private final String BUTTON_NAME_REMOVE_ALL = "Remove All";
 	private final String BUTTON_NAME_GO_TO_RECORDS = "Records";
-	
-	private final String LABEL_NAME_START_SCREEN = "Click to Start";
+
+	private final String LABEL_NAME_START_SCREEN = "Start";
 	private final String LABEL_NAME_GET_READY_SCREEN = "Get Ready";
 	private final String LABEL_NAME_FALSE_START_SCREEN = "Too Soon";
 	private final String LABEL_NAME_CLICK_NOW_SCREEN = "Click Now";
@@ -85,10 +87,10 @@ public class ApplicationView {
 	private final Color COLOR_BUTTON = new Color(50, 50, 50);
 	private final Color COLOR_BUTTON_TEXT = new Color(200, 200, 200);
 	private final Color COLOR_FONT_SCREEN = new Color(255, 255, 255, 150);
-	
+
 	private final int BUTTON_TEXT_SIZE = 15;
 	private final int FONT_SIZE_SCREEN = 100;
-	
+
 	private final Color COLOR_SCREEN_BAR = new Color(0, 0, 0);
 	private final Color COLOR_SCREEN_START = new Color(50, 180, 220);
 	private final Color COLOR_SCREEN_GET_READY = new Color(160, 160, 160);
@@ -96,13 +98,12 @@ public class ApplicationView {
 	private final Color COLOR_SCREEN_CLICK_NOW = new Color(20, 220, 0);
 	private final Color COLOR_RECORDS_BACKGROUND = new Color(50, 180, 220);
 
-
 	private Controller controller;
 	private int randomDelayInMS;
 	private int timeElapsedInMS = 0;
 	private Timer randomDelayTimer;
 	private Timer reactionTimeTimer;
-	
+
 	public static void main(String[] args) {
 		ReactionTimeApplication ata = new ReactionTimeApplication();
 		ApplicationView a = new ApplicationView(ata);
@@ -111,11 +112,14 @@ public class ApplicationView {
 	public ApplicationView(Controller controller) {
 
 		this.controller = controller;
-		frame = createFrame(WINDOW_NAME  , WINDOW_SIZE);
+		frame = createFrame(WINDOW_NAME, WINDOW_SIZE);
+
 		initializePanels();
 		initializeButtons();
 		initializeTimer();
-		setEventAndActionListeners();
+		setKeyListeners();
+		setMouseActionListeners();
+		setButtonActionListeners();
 		setUpMidPanelCards();
 		setUpFrameCards();
 		setAppMidPanels();
@@ -126,15 +130,13 @@ public class ApplicationView {
 	}
 
 
-
-
 	private JFrame createFrame(String name, Dimension WINDOW_SIZE) {
 
 		frame = new JFrame(name);
 		frame.setSize(WINDOW_SIZE);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
+
 		return frame;
 	}
 
@@ -159,126 +161,129 @@ public class ApplicationView {
 		clickPanel = new JPanel();
 	}
 
-	
-
-	
 	private void initializeButtons() {
 
-		goToAppButton 		= createButton(BUTTON_NAME_GO_TO_APP, 		BUTTON_SIZE,BUTTON_TEXT_SIZE);
-		goToRecordsButton 	= createButton(BUTTON_NAME_GO_TO_RECORDS, 	BUTTON_SIZE,BUTTON_TEXT_SIZE);
-		RemoveLastRecordButton = createButton(BUTTON_NAME_REMOVE_LAST, 	BUTTON_SIZE_WIDE,BUTTON_TEXT_SIZE);
-		RemoveAllRecordsButton = createButton(BUTTON_NAME_REMOVE_ALL, 	BUTTON_SIZE_WIDE,BUTTON_TEXT_SIZE);
-	}	
-	
+		goToAppButton = createButton(BUTTON_NAME_GO_TO_APP, BUTTON_SIZE, BUTTON_TEXT_SIZE);
+		goToRecordsButton = createButton(BUTTON_NAME_GO_TO_RECORDS, BUTTON_SIZE, BUTTON_TEXT_SIZE);
+		RemoveLastRecordButton = createButton(BUTTON_NAME_REMOVE_LAST, BUTTON_SIZE_WIDE, BUTTON_TEXT_SIZE);
+		RemoveAllRecordsButton = createButton(BUTTON_NAME_REMOVE_ALL, BUTTON_SIZE_WIDE, BUTTON_TEXT_SIZE);
+	}
+
 	private JButton createButton(String name, Dimension size, int textSize) {
-		
+
 		JButton button = new JButton(name);
 		button.setPreferredSize(size);
 		button.setBorder(new BevelBorder(0));
 		button.setBorderPainted(true);
 		button.setFocusable(false);
 		button.setBackground(COLOR_BUTTON);
-		
+
 		button.setForeground(COLOR_BUTTON_TEXT);
-		button.setFont(new Font(FONT_NAME,1,textSize));
-		
+		button.setFont(new Font(FONT_NAME, 1, textSize));
+
 		return button;
 	}
-	
+
 	private void initializeTimer() {
 		randomDelayTimer = new Timer(100, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				timeElapsedInMS = timeElapsedInMS + 100;
-				System.out.println(timeElapsedInMS);
-				
 				if (timeElapsedInMS > randomDelayInMS) {
-					if (falseStartPanel.isShowing()) {
-						System.out.println("FALSE START IS SHOWING HAHA");
-						
-
-					} else {
 						appMidPanelCardLayout.show(appMidPanelpageController, CARD_NAME_CLICK_PANEL);
-						
-						if(clickPanel.isShowing()) {
-							InitializeReactionSpeedTimer();
-							reactionTimeTimer.start();
-						}
-
-	
-					}
-			
-					timeElapsedInMS = 0;
-					randomDelayTimer.stop();
+						InitializeReactionSpeedTimer();
+						reactionTimeTimer.start();
+						randomDelayTimer.stop();
+						timeElapsedInMS = 0;
 				}
 			}
-
-
 		});
 	}
 
-	
 	private void InitializeReactionSpeedTimer() {
 
 		long initialTimeStamp = System.currentTimeMillis();
-
 		reactionTimeTimer = new Timer(1, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
 				clickNowLabel.setText(Long.toString((System.currentTimeMillis() - initialTimeStamp)));
+			}
+		});
+	}
 
+	private void setKeyListeners() {
+		frame.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+					if (startPanel.isShowing()) {
+						randomDelayInMS = controller.getRandomDelay();
+						randomDelayTimer.start();
+						appMidPanelCardLayout.show(appMidPanelpageController, CARD_NAME_GET_READY_PANEL);
+					}
+					else if(getReadyPanel.isShowing()) {
+						appMidPanelCardLayout.show(appMidPanelpageController, CARD_NAME_FALSE_START_PANEL);
+						timeElapsedInMS = 0;
+						randomDelayTimer.stop();
+						
+					}
+					else if(falseStartPanel.isShowing()) {
+						randomDelayInMS = controller.getRandomDelay();
+						randomDelayTimer.start();
+						appMidPanelCardLayout.show(appMidPanelpageController, CARD_NAME_GET_READY_PANEL);
+						
+					}
+					else if (clickPanel.isShowing()) {
+						reactionTimeTimer.stop();
+						appMidPanelCardLayout.show(appMidPanelpageController, CARD_NAME_START_PANEL);
+						startLabel.setText(clickNowLabel.getText());
+					}
+					
+				}
 			}
 		});
 	}
 	
-	
-	private void setEventAndActionListeners() {
-
+	private void setMouseActionListeners() {
 		startPanel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
-				System.out.println("click");
 				randomDelayInMS = controller.getRandomDelay();
-				System.out.println("random : "+randomDelayInMS);
 				randomDelayTimer.start();
 				appMidPanelCardLayout.show(appMidPanelpageController, CARD_NAME_GET_READY_PANEL);
-
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-
+				
 			}
 		});
-
+		
 		getReadyPanel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
-				System.out.println("FalseStart");
-				randomDelayTimer.stop();
-				timeElapsedInMS = 0;
 				appMidPanelCardLayout.show(appMidPanelpageController, CARD_NAME_FALSE_START_PANEL);
+				timeElapsedInMS = 0;
+				randomDelayTimer.stop();
 			}
 		});
-
+		
 		clickPanel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
-			reactionTimeTimer.stop();
-			appMidPanelCardLayout.show(appMidPanelpageController, CARD_NAME_START_PANEL);
-			startLabel.setText(clickNowLabel.getText());
+				reactionTimeTimer.stop();
+				appMidPanelCardLayout.show(appMidPanelpageController, CARD_NAME_START_PANEL);
+				startLabel.setText(clickNowLabel.getText());
 			}
 		});
-
+		
 		falseStartPanel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
-				startLabel.setText(LABEL_NAME_CLICK_NOW_SCREEN);
-				appMidPanelCardLayout.show(appMidPanelpageController, CARD_NAME_START_PANEL);
+				randomDelayInMS = controller.getRandomDelay();
+				randomDelayTimer.start();
+				appMidPanelCardLayout.show(appMidPanelpageController, CARD_NAME_GET_READY_PANEL);
 			}
 		});
-
+	}
+	
+	private void setButtonActionListeners() {
 		RemoveLastRecordButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -308,9 +313,9 @@ public class ApplicationView {
 		});
 	}
 
-	
+
 	private void setUpMidPanelCards() {
-		
+
 		appMidPanelCardLayout = new CardLayout();
 		appMidPanelpageController.setLayout(appMidPanelCardLayout);
 		midAppPanel.setLayout(new BorderLayout());
@@ -322,7 +327,6 @@ public class ApplicationView {
 		appMidPanelCardLayout.show(appMidPanelpageController, CARD_NAME_START_PANEL);
 	}
 
-	
 	private void setUpFrameCards() {
 
 		cardLayout = new CardLayout();
@@ -333,9 +337,8 @@ public class ApplicationView {
 		frame.add(pageController);
 	}
 
-	
 	private void setAppMidPanels() {
-		
+
 		startLabel = createLabel(LABEL_NAME_START_SCREEN);
 		setPanel(startPanel, COLOR_SCREEN_START, startLabel);
 
@@ -349,26 +352,23 @@ public class ApplicationView {
 		setPanel(clickPanel, COLOR_SCREEN_CLICK_NOW, clickNowLabel);
 	}
 
-	
 	private void setPanel(JPanel panel, Color bg, JLabel label) {
-		
+
 		panel.setBackground(bg);
 		panel.setLayout(new BorderLayout());
 		panel.add(label);
 	}
 
-	
 	private JLabel createLabel(String name) {
-		
+
 		JLabel label = new JLabel(name);
 		label.setForeground(COLOR_FONT_SCREEN);
 		label.setFont(new Font(FONT_NAME, Font.BOLD, FONT_SIZE_SCREEN));
 		label.setHorizontalAlignment(JLabel.CENTER);
-		
+
 		return label;
 	}
 
-	
 	private void setAppPanelComponents() {
 
 		appPanel.setLayout(new BorderLayout());
@@ -388,7 +388,6 @@ public class ApplicationView {
 		appPanel.add(botAppPanel, BorderLayout.SOUTH);
 	}
 
-	
 	private void setRecordsPanelComponents() {
 
 		recordsPanel.setLayout(new BorderLayout());
@@ -410,4 +409,5 @@ public class ApplicationView {
 		recordsPanel.add(midRecordsPanel, BorderLayout.CENTER);
 		recordsPanel.add(botRecordsPanel, BorderLayout.SOUTH);
 	}
+
 }
